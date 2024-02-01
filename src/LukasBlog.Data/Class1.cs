c094fad354585c401a12bbfacd36d9ebbf488d89
Lukas Nguyen <dat.nguyenthaithanh@hotmail.com>
2024-02-01 12:58:04 +0700

    - Created domain and identity objects - Integrated with Entity Framework

--- src/LukasBlog.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LukasBlog.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/LukasBlog.Data/LukasBlogContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukasBlog.Data
+{
+    public class LukasBlogContextFactory : IDesignTimeDbContextFactory<LukasBlogContext>
+    {
+        public LukasBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LukasBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new LukasBlogContext(builder.Options);
+        }
+    }
+}
